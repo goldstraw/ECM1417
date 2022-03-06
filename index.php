5b787a21cd07c6cdab4f800835d53e737d945051
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2022-03-06 10:31:35 +0000

    Added php files

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <ul>
+            <li name="home"><a class="active" href="">Home</a></li>
+            <li name="tetris"><a href="tetris.php">Play Tetris</a></li>
+            <li name="leaderboard"><a href="leaderboard.php">Leaderboard</a></li>
+        </ul>
+        <div class="main">
+            <h1 class="title">Welcome to Tetris</h1>
+            <a href = "tetris.php">
+                <button class="play-button">Click here to play</button>
+            </a>
+        </div>
+    </body>
+</html>
